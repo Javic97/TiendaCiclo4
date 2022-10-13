98ed59d58c47d708e8b22e4a2ef9ff9c143bcf81
Javic97 <111356022+Javic97@users.noreply.github.com>
2022-10-12 23:21:13 -0500

    Commit3

--- back/app.js
+++ back/app.js
@@ -1,4 +1,10 @@
 const express=require("express");
 const app=express();
 
+app.use(express.json());
+
+const productos=require("./rutas/products")
+
+app.use('/api',productos)
+
 module.exports=app
--- back/server.js
+++ back/server.js
@@ -3,6 +3,6 @@ const app=require("./app");
 const dotenv=require("dotenv");
 dotenv.config({path: 'back/config/config.env'})
 
-const server=app.listen(proccess.env.PORT, () =>{
-    console.log(`Servidor iniciado en el puerto: ${Process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+const server=app.listen(process.env.PORT, () =>{
+    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
 })
