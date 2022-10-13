dceb134c54feb200ce5b18c2d10b9c0477145cd9
Javic97 <111356022+Javic97@users.noreply.github.com>
2022-10-12 19:32:31 -0500

    Commit2

--- back/server.js
+++ back/server.js
@@ -1,1 +1,8 @@
-const app=require("app");
+const app=require("./app");
+
+const dotenv=require("dotenv");
+dotenv.config({path: 'back/config/config.env'})
+
+const server=app.listen(proccess.env.PORT, () =>{
+    console.log(`Servidor iniciado en el puerto: ${Process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+})
