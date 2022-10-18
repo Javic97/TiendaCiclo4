12c2ba8a35f93c54b7830d46787a5d22e705aa5d
Javic97 <111356022+Javic97@users.noreply.github.com>
2022-10-18 16:23:43 -0500

    Commit Metodos

--- back/controlador/prodControlador.js
+++ back/controlador/prodControlador.js
@@ -1,9 +1,62 @@
 const producto=require("../modelos/productos")
+const fetch=(url)=>import('node-fetch').then(({default:fecth})=>fetch(url));
 
-exports.getProducts=(req,res,next) =>{
+exports.getProducts=async (req,res,next) =>{
+    const productos = await producto.find();
     res.status(200).json({
         success:true,
-        message:"Ruta OK"
+        count:productos.length,
+        productos
+    })
+}
+
+exports.getProductsById=async (req,res,next) =>{
+    const idproduct=await producto.findById(req.params.id)
+    if(!idproduct){
+        return res.status(404).json({
+            success:false,
+            message:"No se encuentra ese producto"  
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message:"Este es tu producto: ",
+        idproduct
+    })
+}
+
+exports.updateProduct=async(req,res,next) =>{
+    let idproduct=await producto.findById(req.params.id)
+    if(!idproduct){
+        return res.status(404).json({
+            success:false,
+            message:"No se encuentra ese producto"  
+        })
+    }
+    idproduct=await producto.findByIdAndUpdate(req.params.id, req.body, {
+        new:true,
+        runValidators:true
+    });
+    res.status(200).json({
+        success:true,
+        message:"Producto actualizado",
+        idproduct
+    })
+}
+
+exports.deleteProduct=async(req,res,next) =>{
+    const idproduct=await producto.findById(req.params.id)
+    if(!idproduct){
+        return res.status(404).json({
+            success:false,
+            message:"No se encuentra ese producto"  
+        })
+    }
+    await idproduct.remove();
+    res.status(200).json({
+        success:true,
+        message:"Producto eliminado",
+        idproduct
     })
 }
 
@@ -15,3 +68,12 @@ exports.newProduct=async(req,res,next) =>{
         product
     })
 }
+
+function verProductos(){
+    fetch('http://localhost:4000/api/producto')
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+
+verProductos();
--- back/rutas/products.js
+++ back/rutas/products.js
@@ -1,9 +1,12 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts, newProduct}=require("../controlador/prodControlador")
+const {getProducts, newProduct, getProductsById, updateProduct, deleteProduct}=require("../controlador/prodControlador")
 
 router.route('/producto').get(getProducts)
 router.route('/producto/nuevo').post(newProduct)
+router.route('/producto/:id').get(getProductsById)
+router.route('/producto/:id').put(updateProduct)
+router.route('/producto/:id').delete(deleteProduct)
 
 module.exports=router;
