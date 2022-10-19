24a7f92d7735607aa19c52257621ac0fe4806aee
Javic97 <111356022+Javic97@users.noreply.github.com>
2022-10-18 19:54:26 -0500

    Commit front inicial

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda de cafe - Cafe Enigma</h1>
+      <img src="./logo_tienda.png" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
